a9c32ce673d4362c943a05e4a5a2c6fed641818c
njrapidinnovation <nj@rapidinnovation.dev>
2023-08-31 13:09:13 +0530

    FUN-15 changes

--- src/TicoToken.sol
+++ src/TicoToken.sol
@@ -1,37 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-contract Tico {
-    address owner;
+import "@openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin-contracts/contracts/access/Ownable.sol";
 
-    string public constant name = "Funtico";
-    string public constant symbol = "TICO";
-    uint8 public constant decimals = 18;
-    uint public totalSupply = 0;
-
-    mapping(address => uint) public balanceOf;
-    mapping(address => mapping(address => uint)) public allowance;
+contract Tico is ERC20, Ownable {
     mapping(address => bool) public isMinter;
 
     bool public initialMinted;
-    address public redemptionReceiver;
-    address public merkleClaim;
 
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
-
-    constructor() {
-        owner = msg.sender;
+    constructor() ERC20("Funtico", "TICO") {
         isMinter[msg.sender] = true;
         _mint(msg.sender, 0);
     }
 
     // No checks as its meant to be once off to set minting rights to BaseV1 Minter
     function setMinter(address _minter) external {
-        require(msg.sender == owner || isMinter[msg.sender]);
+        require(msg.sender == owner() || isMinter[msg.sender]);
         isMinter[_minter] = true;
     }
-
 
     // // Initial mint: total 82M
     // //  4M for "Genesis" pools
@@ -43,58 +30,8 @@ contract Tico {
     //     _mint(_recipient, 82 * 1e6 * 1e18);
     // }
 
-    function approve(address _spender, uint _value) external returns (bool) {
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function _mint(address _to, uint _amount) internal returns (bool) {
-        totalSupply += _amount;
-        unchecked {
-            balanceOf[_to] += _amount;
-        }
-        emit Transfer(address(0x0), _to, _amount);
-        return true;
-    }
-
-    function _transfer(
-        address _from,
-        address _to,
-        uint _value
-    ) internal returns (bool) {
-        balanceOf[_from] -= _value;
-        unchecked {
-            balanceOf[_to] += _value;
-        }
-        emit Transfer(_from, _to, _value);
-        return true;
-    }
-
-    function transfer(address _to, uint _value) external returns (bool) {
-        return _transfer(msg.sender, _to, _value);
-    }
-
-    function transferFrom(
-        address _from,
-        address _to,
-        uint _value
-    ) external returns (bool) {
-        uint allowed_from = allowance[_from][msg.sender];
-        if (allowed_from != type(uint).max) {
-            allowance[_from][msg.sender] -= _value;
-        }
-        return _transfer(_from, _to, _value);
-    }
-
     function mint(address account, uint amount) external returns (bool) {
-        require(msg.sender == owner || isMinter[msg.sender]);
-        _mint(account, amount);
-        return true;
-    }
-
-    function claim(address account, uint amount) external returns (bool) {
-        require(msg.sender == redemptionReceiver || msg.sender == merkleClaim);
+        require(msg.sender == owner() || isMinter[msg.sender]);
         _mint(account, amount);
         return true;
     }
